0cd4cf1c05ad997976a843ba3443b2d0cd015b33
Mohammed-Benotmane <mohammedbenotmanetom@gmail.com>
2018-09-11 02:04:38 +0200

    add project

---
+++ app/src/main/java/com/example/mohamed/sendmessage/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mohamed.sendmessage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private EditText editText2;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT,editText.getText().toString());
+                intent.putExtra(Intent.EXTRA_TEXT,editText2.getText().toString());
+                startActivity(Intent.createChooser(intent,"Share via"));
+            }
+        });
+
+    }
+}
